b0eb5703cda8a4bc1bc8cb917ad742574e69f4b2
Caio Soares <caio.vasoares@gmail.com>
2021-05-03 16:27:03 -0300

    commit

---
+++ src/routes.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
+import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
+
+const routes = Router();
+
+const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
+
+/**
+ * Tipos de Parametros
+ * Route Params => Parametros de rotas
+ * http://localhost:3333/settings/1
+ * Query Params => Filtros e Buscas
+ * http://localhost:3333/settings/1?search=algumacoisa
+ * 
+ * Body Params => {
+ * 
+ * }
+ */
+
+routes.post("/settings", settingsController.create);
+routes.get("/settings/:username", settingsController.findByUsername);
+routes.put("/settings/:username", settingsController.update);
+
+routes.post("/users", usersController.create);
+
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
+
+
+export { routes };
